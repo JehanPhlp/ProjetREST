28423eeaec37c2c76c2648a89d446a24b4e8c0ef
Benoit <benoit.leclerc@etu.iut-tlse3.fr>
2023-03-09 08:54:57 +0100

    structure de verification de methode

--- serveurApplication.php
+++ serveurApplication.php
@@ -14,4 +14,38 @@
 
     /// Identification du type de méthode HTTP envoyée par le client
     $http_method = $_SERVER['REQUEST_METHOD'];
+
+    switch ($http_method){
+
+        case "GET" :
+            $post=[];
+            
+            break;
+        case "POST" :
+            $postedData = file_get_contents('php://input');
+
+            break;
+        case "PUT" :
+            $postedData = file_get_contents('php://input');
+
+            break;
+        case "DELETE":
+
+            break;
+        default :
+            deliver_response(405, "Methode non implemenee", NULL);
+            break;
+        }
+    
+        function deliver_response($status, $status_message, $data){
+            /// Paramétrage de l'entête HTTP, suite
+            header("HTTP/1.1 $status $status_message");
+            /// Paramétrage de la réponse retournée
+            $response['status'] = $status;
+            $response['status_message'] = $status_message;
+            $response['data'] = $data;
+            /// Mapping de la réponse au format JSON
+            $json_response = json_encode($response);
+            echo $json_response;
+        }
 ?>
