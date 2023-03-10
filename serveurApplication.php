47c5c63d71153932f673c13aab8cd641ce2caf9f
JehanPhlp <93180306+JehanPhlp@users.noreply.github.com>
2023-03-10 11:03:08 +0100

    update appels BDs

--- serveurApplication.php
+++ serveurApplication.php
@@ -1,13 +1,7 @@
 <?php
-    require_once("login.php");
     require_once("jwt_utils.php");
+    require_once("login.php");
 
-    try {
-        $linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
-    }
-    catch (Exception $e) {
-       die('Erreur : ' . $e->getMessage());
-    }
 
     /// Paramétrage de l'entête HTTP (pour la réponse au Client)
     header("Content-Type:application/json");
@@ -28,7 +22,7 @@
             } else {
                 $posts = getPosts();
             }
-            deliver_response(200,"affichage de posts",$post);
+            deliver_response(200,"affichage de posts",json_encode($post));
             break;
         case "POST" :
             $postedData = file_get_contents('php://input');
@@ -41,6 +35,21 @@
             break;
         case "PUT" :
             $postedData = file_get_contents('php://input');
+            $postedDataTab = json_decode($postedData, true);
+            if (empty($_GET['id_post'])){
+                deliver_response(422, "missing parameter : id_post", NULL);
+            }
+            if(!is_jwt_valid($jwt_token)) {
+                deliver_response(401, "token invalide", NULL);
+                break;
+            }
+            if(!is_moderateur($jwt_token) && !is_publisher_of_this_post($jwt_token, $_GET['id_post'])) {
+                deliver_response(401, "vous n'etes pas autorisé à supprimer ce post");
+            }
+            if(isVide($postedDataTab['contenu'])) {
+                deliver_response(384, "contenu article vide", NULL);
+                break;
+            }
 
             break;
         case "DELETE":
@@ -55,7 +64,7 @@
                 deliver_response(401, "vous n'etes pas autorisé à supprimer ce post");
             }            
 
-            $req = $linkpdo->prepare('DELETE from post where id_Post = ?');
+            $req = createDB()->prepare('DELETE from post where id_Post = ?');
             $req->execute(array($_GET['id_post']));
         
             /// Envoi de la réponse au Client
@@ -81,10 +90,16 @@
         $tokenParts = explode('.', $jwt_token);
         $payload = base64_decode($tokenParts[1]);
         $username = json_decode($payload)->username;
-
-        $req = $linkpdo->prepare('SELECT p.id_utilisateur FROM utilisateur u, post p WHERE u.id_utilisateur = p.id_utilisateur AND u.nom = ? AND p.id_post = ?');
-        $req->execute(array($username, $id_post));
-        $reponseBD = $req->fetchAll(PDO::FETCH_ASSOC);
+        
+        try {
+            $req = createDB()->prepare('SELECT p.id_utilisateur FROM utilisateur u, post p WHERE u.id_utilisateur = p.id_utilisateur AND u.nom = ? AND p.id_post = ?');
+            $req->execute(array($username, $id_post));
+            $reponseBD = $req->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+        
 
         return(count($reponseBD) > 0);
     }
@@ -117,7 +132,7 @@
 
     function getPosts(){
         try {
-            $select = $linkpdo->prepare('SELECT * FROM post');
+            $select = createDB()->prepare('SELECT * FROM post');
             $select->execute();
             $posts = $select->fetchAll(PDO::FETCH_ASSOC);
             return $posts;
@@ -129,7 +144,7 @@
 
     function getPost($id){
         try {
-            $select = $linkpdo->prepare('SELECT * FROM post WHERE id = ?');
+            $select = createDB()->prepare('SELECT * FROM post WHERE id = ?');
             $select->execute(array($id));
             $post = $select->fetchAll(PDO::FETCH_ASSOC);
             return $post;
@@ -141,7 +156,7 @@
 
     function getPostFromUser($username){
         try{
-            $select = $linkpdo->prepare('SELECT * FROM post,utilisateur as u WHERE post.Id_Utilisateur=u.Id_Utilisateur and u.nom=?');
+            $select = createDB()->prepare('SELECT * FROM post,utilisateur as u WHERE post.Id_Utilisateur=u.Id_Utilisateur and u.nom=?');
             $select->execute(array($username));
             $posts = $select->fetchAll(PDO::FETCH_ASSOC);
             return $posts;
@@ -151,10 +166,10 @@
         }
     }
 
-    function creatPost($username,$contenue){
+    function creatPost($username,$contenu){
         try {
-            $req = $linkpdo->prepare('INSERT INTO post(contenu,date_publication,Id_Utilisateur) values(?,?,?)');
-            $req->execute(array($contenue),time(),getIdUserFromUsername($username));
+            $req = createDB()->prepare('INSERT INTO post(contenu,date_publication,Id_Utilisateur) values(?,?,?)');
+            $req->execute(array($contenu ,time(),getIdUserFromUsername($username)));
         } catch(Exception $e) {
             echo"erreur";
             die('Erreur:'.$e->getMessage());
@@ -162,7 +177,7 @@
     }
     function getIdUserFromUsername($username){
         try{
-            $select = $linkpdo->prepare('SELECT Id_Utilisateur FROM utilisateur as u WHERE u.nom=?');
+            $select = createDB()->prepare('SELECT Id_Utilisateur FROM utilisateur as u WHERE u.nom=?');
             $select->execute(array($username));
             $posts = $select->fetchAll(PDO::FETCH_ASSOC);
             return $posts;
@@ -170,5 +185,20 @@
             echo"erreur";
             die('Erreur:'.$e->getMessage());
         }
+    }
+
+    function createDB(){
+        global $server;
+        global $db;
+        global $login;
+        global $mdp;
+        try {
+            $linkpdo = new PDO("mysql:host=$server;dbname=$db;charset=UTF8", $login, $mdp);
+            $linkpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+        return $linkpdo;
     }
 ?>
